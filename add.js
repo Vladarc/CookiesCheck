49fff2d072d85b2d98daaeeb9c3b1c53f2de5a99
Vladarc <vladmironecdonnu@gmail.com>
2020-05-04 19:52:16 +0300

    first commit

---
+++ add.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded',function(){
+  let modal =   document.querySelector('.modal')
+  let modalBtn =   document.querySelector('.modal-button_reload')
+
+    const cookiesCheck = () =>{
+        if(navigator.cookieEnabled === false ){
+            modal.classList.add('active')
+            document.body.classList.add('active--hiden')
+        }
+    }
+
+ /*    const handler = (event)=> {
+        event.preventDefault()
+        document.location.reload()
+    }
+
+    modalBtn.addEventListener('click',handler) */
+    
+
+   
+
+
+
+    cookiesCheck()
+})
+
