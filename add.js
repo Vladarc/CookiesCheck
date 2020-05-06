f76db573e11b05eaa1ee2413c1a74857ecc685ff
Vladarc <vladmironecdonnu@gmail.com>
2020-05-06 14:42:38 +0300

    second commit

--- add.js
+++ add.js
@@ -1,26 +1,38 @@
-document.addEventListener('DOMContentLoaded',function(){
-  let modal =   document.querySelector('.modal')
-  let modalBtn =   document.querySelector('.modal-button_reload')
 
-    const cookiesCheck = () =>{
-        if(navigator.cookieEnabled === false ){
-            modal.classList.add('active')
-            document.body.classList.add('active--hiden')
-        }
-    }
+(function() {
+    "use strict"
+    let modal =   document.querySelector('.modal')
+    let modalBtn =   document.querySelector('.modal-button_reload')
+  
+      const cookiesCheck = () =>{
+          if(navigator.cookieEnabled === true ){
+              modal.classList.add('active')
+              document.body.classList.add('active--hiden')
+          }
+      }
+      
+      window.addEventListener('storage',cookiesCheck)
+  
+  
+      const handler = (event)=> {
+          event.preventDefault()
+          document.location.reload()
+      }
+  
+      modalBtn.addEventListener('click',handler)
+  
+  window.addEventListener('onpageshow',function() {
+      console.log(1)
+  })
+     
+  
+  
+  
+    cookiesCheck() 
 
-    const handler = (event)=> {
-        event.preventDefault()
-        document.location.reload()
-    }
-
-    modalBtn.addEventListener('click',handler)
+})()
     
-
-   
-
+      
 
 
-    cookiesCheck()
-})
 
